02760e293c696923fc2bd18ac04bbfbd821c75b8
quangMor <thangteoxx@gmail.com>
2023-03-09 10:19:11 +0700

    laborreport

--- layouts/Home.vue
+++ layouts/Home.vue
@@ -2,13 +2,17 @@
   <div>
     <Header />
     <Nuxt />
+    <Foodter class="fixed bottom-0 right-0 mr-2 mb-2" />
   </div>
 </template>
 <script>
 import Header from "@/components/Home/Header.vue";
+import Foodter from "@/components/Home/Foodter.vue";
+
 export default {
   components: {
     Header,
+    Foodter,
   },
 };
 </script>
---
+++ pages/laborreport/index.vue
@@ -0,0 +1,33 @@
+<template lang="">
+  <div class="container">
+    <div class="text-center mt-10 mb-24 text-3xl font-bold title">
+      BÁO CÁO SỐ LAO ĐỘNG NGHỈ
+    </div>
+
+    <div class="max-w-2xl m-auto m-0">
+      <Input :label="label" />
+    </div>
+  </div>
+</template>
+<script>
+import Input from "@/components/input/Input.vue";
+export default {
+  layout: "Home",
+  components: {
+    Input,
+  },
+  data() {
+    return {
+      label: {
+        name: "Nhập số lao động nghỉ :",
+        placeholder: "số lao động nghỉ",
+      },
+    };
+  },
+};
+</script>
+<style scoped>
+.title {
+  color: #0daeff;
+}
+</style>
