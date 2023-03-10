fabe2cb468895f7b0b587c5f8998746d2f10ab24
trungquang <thangteoxx@gmail.com>
2023-03-10 20:44:31 +0700

    edit table

--- components/tableInformation/TableInformation.vue
+++ components/tableInformation/TableInformation.vue
@@ -7,51 +7,48 @@
     </tr>
     <tr>
       <td class="bg-sky-400 text-slate-50">ĐỊNH BIÊN</td>
-      <td>{{ dataTable.demarcation.quantity }}</td>
-      <td>{{ dataTable.demarcation.information }}</td>
+      <td>{{ demarcation.quantity }}</td>
+      <td>{{ demarcation.information }}</td>
     </tr>
     <tr>
       <td class="bg-sky-400 text-slate-50">SỐ THỜI VỤ</td>
-      <td>{{ dataTable.numberSeasonal.quantity }}</td>
-      <td>{{ dataTable.numberSeasonal.information }}</td>
+      <td>{{ numberSeasonal.quantity }}</td>
+      <td>{{ numberSeasonal.information }}</td>
     </tr>
     <tr>
       <td class="bg-sky-400 text-slate-50">SỐ H.SINH</td>
-      <td>{{ dataTable.numberStudent.quantity }}</td>
-      <td>{{ dataTable.numberStudent.information }}</td>
+      <td>{{ numberStudent.quantity }}</td>
+      <td>{{ numberStudent.information }}</td>
     </tr>
     <tr>
       <td class="bg-sky-400 text-slate-50">ĐIỀU CHUYỂN</td>
-      <td>{{ dataTable.numberTransfer.quantity }}</td>
-      <td>{{ dataTable.numberTransfer.information }}</td>
+      <td>{{ numberTransfer.quantity }}</td>
+      <td>{{ numberTransfer.information }}</td>
     </tr>
     <tr>
       <td class="bg-sky-400 text-slate-50">HỖ TRỢ</td>
-      <td>{{ dataTable.numberSupport.quantity }}</td>
-      <td>{{ dataTable.numberSupport.information }}</td>
+      <td>{{ numberSupport.quantity }}</td>
+      <td>{{ numberSupport.information }}</td>
     </tr>
     <tr>
       <td class="bg-sky-400 text-slate-50">BÁO CƠM</td>
-      <td>{{ dataTable.numberEatRice.quantity }}</td>
-      <td>{{ dataTable.numberEatRice.information }}</td>
+      <td>{{ numberEatRice.quantity }}</td>
+      <td>{{ numberEatRice.information }}</td>
     </tr>
 
     <tr>
       <td class="bg-sky-400 text-slate-50">SỐ NGHỈ</td>
-      <td>{{ dataTable.numberReasons.quantity }}</td>
+      <td>{{ numberReasons.quantity }}</td>
       <td>
-        <div
-          v-for="(item, index) in dataTable.numberReasons.arrReasons"
-          :key="index"
-        >
+        <div v-for="(item, index) in numberReasons.arrReasons" :key="index">
           {{ item.name }}-{{ item.report }}
         </div>
       </td>
     </tr>
     <tr>
       <td class="bg-orange-400 text-slate-50">SỐ LĐ TÍNH NĂNG SUẤT</td>
-      <td>{{ dataTable.numberProductivity.quantity }}</td>
-      <td>{{ dataTable.numberProductivity.information }}</td>
+      <td>{{ numberProductivity.quantity }}</td>
+      <td>{{ numberProductivity.information }}</td>
     </tr>
   </table>
 </template>
@@ -59,24 +56,22 @@
 export default {
   setup() {
     return {
-      dataTable: {
-        numberReasons: {
-          quantity: 3,
-          arrReasons: [
-            { name: "trungquang", report: "om" },
-            { name: "trungquang", report: "om" },
-            { name: "trungquang", report: "om" },
-          ],
-        },
-
-        demarcation: { information: "abc", quantity: 100 },
-        numberSeasonal: { information: "", quantity: 54 },
-        numberStudent: { information: "", quantity: 3 },
-        numberTransfer: { information: "", quantity: 3 },
-        numberSupport: { information: "", quantity: 5 },
-        numberEatRice: { information: "", quantity: 6 },
-        numberProductivity: { information: "", quantity: 43 },
+      numberReasons: {
+        quantity: 3,
+        arrReasons: [
+          { name: "trungquang", report: "om" },
+          { name: "trungquang", report: "om" },
+          { name: "trungquang", report: "om" },
+        ],
       },
+
+      demarcation: { information: "abc", quantity: 100 },
+      numberSeasonal: { information: "", quantity: 54 },
+      numberStudent: { information: "", quantity: 3 },
+      numberTransfer: { information: "", quantity: 3 },
+      numberSupport: { information: "", quantity: 5 },
+      numberEatRice: { information: "", quantity: 6 },
+      numberProductivity: { information: "", quantity: 43 },
     };
   },
 };
