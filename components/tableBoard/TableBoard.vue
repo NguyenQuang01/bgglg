ba142e670ed3c60f6ded3958852022615e8ad3e1
quangMor <thangteoxx@gmail.com>
2023-03-14 08:57:13 +0700

    av

--- components/tableBoard/TableBoard.vue
+++ components/tableBoard/TableBoard.vue
@@ -3,9 +3,8 @@
     <table>
       <tr>
         <th>Bộ phận</th>
-        <th>Tổng số lao động</th>
-        <th>Lao động báo năng suất</th>
-        <th>Lao động báo năng suất</th>
+        <th>VP</th>
+        <th>XN</th>
         <th>Lao động báo năng suất</th>
         <th>Tỉ lệ %</th>
         <th>Tổng lao động báo năng suất</th>
@@ -14,7 +13,6 @@
         <td class="font-bold backgroundBlue">văn phòng</td>
         <td>191 <span class="text-emerald-700"></span></td>
         <td></td>
-        <td>191<span class="text-emerald-700"></span></td>
         <td>191<span class="text-emerald-700"></span></td>
         <td>11.39 <span class="text-rose-700"></span></td>
         <td rowspan="3" class="font-bold">
@@ -25,7 +23,6 @@
         <td class="font-bold backgroundBlue">Tổ may + thời vụ</td>
         <td></td>
         <td>1197.5 <span class="text-emerald-700"></span></td>
-        <td>191<span class="text-emerald-700"></span></td>
         <td>1197.5<span class="text-emerald-700"></span></td>
         <td>71.10<span class="text-emerald-700"></span></td>
       </tr>
@@ -33,14 +30,12 @@
         <td class="font-bold backgroundBlue">Đơn vị lẻ</td>
         <td></td>
         <td>293.5<span class="text-emerald-700"></span></td>
-        <td>191<span class="text-emerald-700"></span></td>
         <td>293.5 <span class="text-emerald-700"></span></td>
         <td><span class="text-emerald-700">17.51</span></td>
       </tr>
 
       <tr>
         <td class="font-bold backgroundBlue">Tổng thực tế làm việc</td>
-        <td class="font-bold">191<span class="text-rose-700"></span></td>
         <td class="font-bold">191 <span class="text-rose-700"></span></td>
         <td colspan="2" class="font-bold">1501.0</td>
         <td class="font-bold">100</td>
