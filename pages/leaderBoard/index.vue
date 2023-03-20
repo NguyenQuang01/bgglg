87ef631455db2600499b15a4a25669e48c3d5fab
quangMor <quangMor@mor.com.vn>
2023-03-20 10:47:05 +0700

    aff

--- pages/leaderBoard/index.vue
+++ pages/leaderBoard/index.vue
@@ -3,13 +3,14 @@
     <div class="text-center mt-16 text-3xl font-bold title mb-16">
       Bảng báo cáo
     </div>
-    <b-button   @click="$router.back()"  class="text-blue-500 border-none p-0"
-          ><b-icon icon="arrow-bar-left" aria-hidden="true"></b-icon>trở lại</a
-        ></b-button>
+    <b-button @click="$router.back()" class="text-blue-500 border-none p-0"
+      ><b-icon icon="arrow-bar-left" aria-hidden="true"></b-icon>trở
+      lại</b-button
+    >
     <div class="mb-2 flex">
       <a-button class="btnDay">Ngày trước</a-button>
       <a-date-picker v-model="valueDay" class="mx-2" placeholder="Chọn ngày" />
-      <a-button class="btnDay">ngày sau</a-button>
+      <a-button class="btnDay" @click="btnPlus">ngày sau</a-button>
     </div>
     <div class="m-auto m-0 overflow-auto">
       <TableBoardVue />
@@ -27,18 +28,34 @@ export default {
   },
   data() {
     return {
-      valueDay: "",
       day: new Date().getDate(),
       month: new Date().getMonth() + 1,
       year: new Date().getFullYear(),
+      day30: [4, 6, 9, 11],
+      day31: [1, 3, 5, 7, 8, 10, 12],
+      day28: [2],
+      valueDay: "",
     };
   },
   created() {
-    this.valueDay = `${this.day}/${this.month}/${this.year}` || "";
+    this.valueDay = `${this.year}-${this.month}-${this.day}`;
   },
   methods: {
     btnPageToDetail() {
       this.$router.push("/leaderDetailBoard");
+    },
+    btnPlus() {
+      if (this.day31.includes(this.month) && this.day < 31) {
+        this.day = this.day + 1;
+        this.valueDay = `${this.year}-${this.month}-${this.day}`;
+        console.log(this.day);
+      } else if (this.day30.includes(this.month) && this.day < 30) {
+        this.day = this.day + 1;
+        this.valueDay = `${this.year}-${this.month}-${this.day}`;
+      } else if (this.day28.includes(this.month) && this.day < 28) {
+        this.day = this.day + 1;
+        this.valueDay = `${this.year}-${this.month}-${this.day}`;
+      }
     },
   },
   // watch: {
