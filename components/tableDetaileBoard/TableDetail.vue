737207607529268a02c884be318b1966a2c992de
quangMor <quangMor@mor.com.vn>
2023-03-27 17:30:20 +0700

    33

--- components/tableDetaileBoard/TableDetail.vue
+++ components/tableDetaileBoard/TableDetail.vue
@@ -58,25 +58,25 @@ const data = [
     key: 1,
     name: "văn phòng",
     age: 60,
-    address: "New York No. 1 Lake Park",
+    address: "88",
     children: [
       {
         key: 11,
         name: "Lãnh đạo",
         age: 42,
-        address: "New York No. 2 Lake Park",
+        address: "99",
       },
       {
         key: 12,
         name: "Phòng TCHC",
         age: 30,
-        address: "New York No. 3 Lake Park",
+        address: "5",
       },
       {
         key: 13,
         name: "Kế Toán",
         age: 72,
-        address: "London No. 1 Lake Park",
+        address: "8",
       },
     ],
   },
@@ -84,19 +84,19 @@ const data = [
     key: 2,
     name: "Đơn vị lẻ",
     age: 32,
-    address: "Sidney No. 1 Lake Park",
+    address: "77",
     children: [
       {
         key: 11,
         name: "XN1",
         age: 42,
-        address: "New York No. 2 Lake Park",
+        address: "99",
         children: [
           {
             key: 11,
             name: "Vệ Sinh 1",
             age: 42,
-            address: "New York No. 2 Lake Park",
+            address: "99",
           },
         ],
       },
@@ -104,7 +104,7 @@ const data = [
         key: 12,
         name: "XN2",
         age: 42,
-        address: "New York No. 2 Lake Park",
+        address: "99",
       },
     ],
   },
@@ -112,7 +112,7 @@ const data = [
     key: 2,
     name: "Tổ May	",
     age: 32,
-    address: "Sidney No. 1 Lake Park",
+    address: "77",
   },
 ];
 const rowSelection = {};
