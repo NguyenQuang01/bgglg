090a471b6db072509bc016c444a9339b4b211251
quangMor <quangMor@mor.com.vn>
2023-03-28 11:19:10 +0700

    a

--- components/tableDetaileBoard/TableDetail.vue
+++ components/tableDetaileBoard/TableDetail.vue
@@ -2,181 +2,174 @@
   <a-table :columns="columns" :data-source="data" bordered />
 </template>
 <script>
-import { defineComponent } from "vue";
-const columns = [
-  {
-    title: "Bộ phận",
-    dataIndex: "name",
-    key: "name",
-  },
-
-  {
-    title: "Văn phòng",
-    dataIndex: "office",
-    // width: "12%",
-  },
-  {
-    title: "Xí nghiệp",
-    dataIndex: "enterprise",
-  },
-  {
-    title: "Lao động báo năng suất",
-    dataIndex: "laborProductivity",
-  },
-  {
-    title: "Số lao động nghỉ",
-    dataIndex: "NumberLeave",
-  },
-  {
-    title: "Tỉ lệ %",
-    dataIndex: "Ratio",
-  },
-  {
-    title: "Tổng lao động báo năng suất",
-    dataIndex: "totalLaborProductivity",
-  },
-  {
-    title: "Báo cơm",
-    dataIndex: "NumberRice",
-  },
-];
-const data = [
-  {
-    key: 1,
-    name: "văn phòng",
-    office: 60,
-    enterprise: "88",
-    laborProductivity: 33,
-    NumberLeave: 22,
-    Ratio: 99,
-    totalLaborProductivity: 55,
-    NumberRice: 11,
-    children: [
-      {
-        key: 11,
-        name: "Lãnh đạo",
-        office: 42,
-        enterprise: "99",
-        laborProductivity: 33,
-        NumberLeave: 22,
-        Ratio: 99,
-        totalLaborProductivity: 55,
-        NumberRice: 11,
-      },
-      {
-        key: 12,
-        name: "Phòng TCHC",
-        office: 30,
-        enterprise: "5",
-        laborProductivity: 33,
-        NumberLeave: 22,
-        Ratio: 99,
-        totalLaborProductivity: 55,
-        NumberRice: 11,
-      },
-      {
-        key: 13,
-        name: "Kế Toán",
-        office: 72,
-        enterprise: "8",
-        laborProductivity: 33,
-        NumberLeave: 22,
-        Ratio: 99,
-        totalLaborProductivity: 55,
-        NumberRice: 11,
-      },
-    ],
-  },
-  {
-    key: 2,
-    name: "Đơn vị lẻ",
-    office: 32,
-    enterprise: "77",
-    laborProductivity: 33,
-    NumberLeave: 22,
-    Ratio: 99,
-    totalLaborProductivity: 55,
-    NumberRice: 11,
-    children: [
-      {
-        key: 131,
-        name: "XN1",
-        office: 42,
-        enterprise: "99",
-        laborProductivity: 33,
-        NumberLeave: 22,
-        Ratio: 99,
-        totalLaborProductivity: 55,
-        NumberRice: 11,
-        children: [
-          {
-            key: 111,
-            name: "Vệ Sinh 1",
-            office: 42,
-            enterprise: "99",
-            laborProductivity: 33,
-            NumberLeave: 22,
-            Ratio: 99,
-            totalLaborProductivity: 55,
-            NumberRice: 11,
-          },
-        ],
-      },
-      {
-        key: 1222,
-        name: "XN2",
-        office: 42,
-        enterprise: "99",
-        laborProductivity: 33,
-        NumberLeave: 22,
-        Ratio: 99,
-        totalLaborProductivity: 55,
-        NumberRice: 11,
-      },
-    ],
-  },
-  {
-    key: 2212,
-    name: "Tổ May	",
-    office: 32,
-    enterprise: "77",
-    laborProductivity: 33,
-    NumberLeave: 22,
-    Ratio: 99,
-    totalLaborProductivity: 55,
-    NumberRice: 11,
-  },
-];
-const rowSelection = {};
-export default defineComponent({
-  setup() {
+export default {
+  data() {
     return {
-      data,
-      columns,
-      rowSelection,
+      data: [
+        {
+          key: 1,
+          name: "văn phòng",
+          office: 60,
+          enterprise: "88",
+          laborProductivity: 33,
+          NumberLeave: 22,
+          Ratio: 99,
+          totalLaborProductivity: 55,
+          NumberRice: 11,
+          children: [
+            {
+              key: 11,
+              name: "Lãnh đạo",
+              office: 42,
+              enterprise: "99",
+              laborProductivity: 33,
+              NumberLeave: 22,
+              Ratio: 99,
+              totalLaborProductivity: 55,
+              NumberRice: 11,
+            },
+            {
+              key: 12,
+              name: "Phòng TCHC",
+              office: 30,
+              enterprise: "5",
+              laborProductivity: 33,
+              NumberLeave: 22,
+              Ratio: 99,
+              totalLaborProductivity: 55,
+              NumberRice: 11,
+            },
+            {
+              key: 13,
+              name: "Kế Toán",
+              office: 72,
+              enterprise: "8",
+              laborProductivity: 33,
+              NumberLeave: 22,
+              Ratio: 99,
+              totalLaborProductivity: 55,
+              NumberRice: 11,
+            },
+          ],
+        },
+        {
+          key: 2,
+          name: "Đơn vị lẻ",
+          office: 32,
+          enterprise: "77",
+          laborProductivity: 33,
+          NumberLeave: 22,
+          Ratio: 99,
+          totalLaborProductivity: 55,
+          NumberRice: 11,
+          children: [
+            {
+              key: 131,
+              name: "XN1",
+              office: 42,
+              enterprise: "99",
+              laborProductivity: 33,
+              NumberLeave: 22,
+              Ratio: 99,
+              totalLaborProductivity: 55,
+              NumberRice: 11,
+              children: [
+                {
+                  key: 111,
+                  name: "Vệ Sinh 1",
+                  office: 42,
+                  enterprise: "99",
+                  laborProductivity: 33,
+                  NumberLeave: 22,
+                  Ratio: 99,
+                  totalLaborProductivity: 55,
+                  NumberRice: 11,
+                },
+              ],
+            },
+            {
+              key: 1222,
+              name: "XN2",
+              office: 42,
+              enterprise: "99",
+              laborProductivity: 33,
+              NumberLeave: 22,
+              Ratio: 99,
+              totalLaborProductivity: 55,
+              NumberRice: 11,
+            },
+          ],
+        },
+        {
+          key: 2212,
+          name: "Tổ May	",
+          office: 32,
+          enterprise: "77",
+          laborProductivity: 33,
+          NumberLeave: 22,
+          Ratio: 99,
+          totalLaborProductivity: 55,
+          NumberRice: 11,
+        },
+      ],
+      columns: [
+        {
+          title: "Bộ phận",
+          dataIndex: "name",
+          key: "name",
+        },
+
+        {
+          title: "Văn phòng",
+          dataIndex: "office",
+          // width: "12%",
+        },
+        {
+          title: "Xí nghiệp",
+          dataIndex: "enterprise",
+        },
+        {
+          title: "Lao động báo năng suất",
+          dataIndex: "laborProductivity",
+        },
+        {
+          title: "Số lao động nghỉ",
+          dataIndex: "NumberLeave",
+        },
+        {
+          title: "Tỉ lệ %",
+          dataIndex: "Ratio",
+        },
+        {
+          title: "Tổng lao động báo năng suất",
+          dataIndex: "totalLaborProductivity",
+        },
+        {
+          title: "Báo cơm",
+          dataIndex: "NumberRice",
+        },
+      ],
     };
   },
-});
+};
 </script>
 <style>
 .ant-table-thead > tr > th {
-  background-color: #0daeff;
-  color: #ffff;
+  background-color: #0daeff !important;
+  color: #ffff !important;
 }
 /* .ant-table-tbody > tr > td {
   background-color: tomato;
 } */
-.red {
-  color: red;
-}
+
 .ant-table-tbody > tr > td:first-child {
-  background-color: #0daeff;
-  color: #ffff;
-  white-space: nowrap;
+  background-color: #0daeff !important;
+  color: #ffff !important;
+  white-space: nowrap !important;
 }
 
 .ant-table-tbody > tr.ant-table-row:hover > td {
   /* background: none !important; */
-  color: rgba(0, 0, 0, 0.65);
+  color: rgba(0, 0, 0, 0.65) !important;
 }
 </style>
--- pages/reasonForLeave/index.vue
+++ pages/reasonForLeave/index.vue
@@ -22,10 +22,8 @@
           </b-form-group>
 
           <div class="mb-2 flex">
-            <b-button type="submit" variant="primary text-blue-700 "
-              >thêm</b-button
-            >
-            <b-button
+            <b-button type="submit" class="add-btn">thêm</b-button>
+            <!-- <b-button
               variant="danger"
               class="text-rose-700 mx-2"
               @click="deletes"
@@ -36,12 +34,12 @@
               class="text-yellow-700 mx-2"
               @click="edit"
               >sửa</b-button
-            >
+            > -->
           </div>
         </b-form>
       </div>
       <div>
-        <a-table
+        <!-- <a-table
           class="mb-10"
           :row-selection="{
             selectedRowKeys: selectedRowKeys,
@@ -49,14 +47,49 @@
           }"
           :columns="columns"
           :data-source="data.value"
-        />
+        /> -->
+        <!-- <b-table striped :items="data.value"></b-table> -->
+        <table id="customers">
+          <tr>
+            <th>Sl</th>
+            <th>Lý do</th>
+            <th></th>
+          </tr>
+          <tr v-for="(item, index) in data.value" :key="index">
+            <td>{{ index + 1 }}</td>
+            <td>
+              <b-form-input
+                v-model="item.name"
+                class="w-20 h-8"
+                v-if="isEdit.value"
+              />
+              <div v-else>{{ item.name }}</div>
+            </td>
+            <td style="text-align: center">
+              <div>
+                <b-button class="save" @click="save(item.name, item.id)"
+                  >lưu</b-button
+                >
+                <b-button class="edit" @click="edit">sửa</b-button
+                ><b-button class="delete" @click="deletes(item.id)"
+                  >xóa</b-button
+                >
+              </div>
+            </td>
+          </tr>
+        </table>
       </div>
     </div>
     <div></div>
   </div>
 </template>
 <script>
-import { reason, deleteLeave, addLeave } from "@/api/AuthenConnector.js";
+import {
+  reason,
+  deleteLeave,
+  addLeave,
+  editLeave,
+} from "@/api/AuthenConnector.js";
 import {
   useRoute,
   computed,
@@ -71,6 +104,7 @@ import { message } from "ant-design-vue";
 export default defineComponent({
   middleware: "auth",
   setup() {
+    const isEdit = reactive({ value: false });
     const form = reactive({
       name: "",
     });
@@ -103,14 +137,17 @@ export default defineComponent({
     };
     const getvalue = async () => {
       const res = await reason();
-      data.value = res.data.map((item) => ({ key: item.id, name: item.name }));
+      data.value = res.data.map((item, index) => ({
+        SL: index + 1,
+        name: item.name,
+        id: item.id,
+      }));
     };
-    getvalue();
-    const deletes = async () => {
-      const res = await deleteLeave(idItem.value);
+    const deletes = async (id) => {
+      const res = await deleteLeave(id);
       if (res && res.code === 200) {
         message.success("xóa thành công");
-        window.location.reload();
+        getvalue();
       }
     };
     const onSubmit = async (event) => {
@@ -118,16 +155,24 @@ export default defineComponent({
       const res = await addLeave([{ name: form.name }]);
       if (res && res.code === 201) {
         message.success("thêm thành công");
-        window.location.reload();
+        getvalue();
       }
     };
     const edit = () => {
-      window.location.href = `/reasonForLeave/${idItem.value}`;
+      isEdit.value = !isEdit.value;
+    };
+    const save = async (name, id) => {
+      const res = await editLeave(name, id);
+      if (res.code === 200 && res) {
+        message.success("sửa thành công");
+      }
+      isEdit.value = !isEdit.value;
     };
     onMounted(() => {
       getvalue();
     });
     return {
+      isEdit,
       form,
       getvalue,
       data,
@@ -138,7 +183,27 @@ export default defineComponent({
       onSubmit,
       deletes,
       edit,
+      save,
     };
   },
 });
 </script>
+<style scoped>
+.save {
+  background-color: rgba(0, 201, 0, 0.8);
+  margin-right: 5px;
+  padding: 2px 7px;
+}
+.edit {
+  background-color: rgba(201, 198, 0, 0.8);
+  margin-right: 5px;
+  padding: 2px 5px;
+}
+.delete {
+  background-color: rgba(201, 74, 0, 0.8);
+  padding: 2px 8px;
+}
+.add-btn {
+  background-color: #1890ff;
+}
+</style>
