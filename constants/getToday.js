eed7a00bef19dfaffb8b55c9ccdccaaca8189310
quangMor <thangteoxx@gmail.com>
2023-06-01 09:07:13 +0700

    fix

--- constants/getToday.js
+++ constants/getToday.js
@@ -1,8 +1,12 @@
+import dayjs from "dayjs";
+
 const today = () => {
   const day = new Date().getDate();
   const month = (new Date().getMonth() + 1).toString().padStart(2, "0");
   const year = new Date().getFullYear();
-  return `${year}/${month}/${day}`;
+  const ymd = `${year}/${month}/${day}`;
+  const format = dayjs(ymd).format("YYYY/MM/DD");
+  return format;
 };
 
 export { today };
